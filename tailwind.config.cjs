554afce5ab295263ca38c15f632a61e3c3e2b93b
Blair Fleming <blair3003.bf@gmail.com>
2023-07-09 11:50:08 +0100

    fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,11 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'space': ['"Space Mono"', 'monospace'],
+			'fira': ['"Fira Sans"', 'sans-serif'],
+			'fira-extra': ['"Fira Sans Extra Condensed"', 'sans-serif'],
+		},
 	},
 	plugins: [],
 }
